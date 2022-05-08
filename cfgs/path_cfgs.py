31ccf09aa285ce53989a2c5247a9200f8aeacb7e
BrightQin <64759750+BrightQin@users.noreply.github.com>
2022-05-08 16:30:14 +0800

    Update path_cfgs.py

--- cfgs/path_cfgs.py
+++ cfgs/path_cfgs.py
@@ -1,9 +1,3 @@
-# --------------------------------------------------------
-# mcan-vqa (Deep Modular Co-Attention Networks)
-# Licensed under The MIT License [see LICENSE for details]
-# Written by Yuhao Cui https://github.com/cuiyuhao1996
-# --------------------------------------------------------
-
 import os
 
 class PATH:
